e44b7388c2aac213e8ee42d9a46fddf853be3913
Anderson Patricio <anderson@patricio.ca>
2019-11-06 11:14:47 -0500

    Update EncryptVM.ps1

--- EncryptVM.ps1
+++ EncryptVM.ps1
@@ -1,17 +1,6 @@
 Function EncryptVM($KeyVault,$KeyName,$VM){
 
     $kv = Get-azKeyVault -VaultName $KeyVault
-
     $key = get-Azkeyvaultkey -Name $KeyName -VaultName $KeyVault
-
-    Set-AzVmDiskEncryptionExtension -ResourceGroupName $kv.ResourceGroupName `
-
-    -DiskEncryptionKeyVaultId  $kv.ResourceID `
-
-    -DiskEncryptionKeyVaultUrl $kv.VaultURI `
-
-    -VMName $VM -KeyEncryptionKeyVaultId $kv.ResourceID `
-
-    -KeyEncryptionKeyUrl $key.id -SkipVmBackup -VolumeType "All"
-
+    Set-AzVmDiskEncryptionExtension -ResourceGroupName $kv.ResourceGroupName -DiskEncryptionKeyVaultId  $kv.ResourceID -DiskEncryptionKeyVaultUrl $kv.VaultURI -VMName $VM -KeyEncryptionKeyVaultId $kv.ResourceID -KeyEncryptionKeyUrl $key.id -SkipVmBackup -VolumeType "All"
 }
